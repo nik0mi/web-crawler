635607a10feee420f4f2cf1cbee605d6b70f48f8
Roman Loskutov <mxhpns@gmail.com>
2023-05-26 16:32:07 +0700

    nothing2

--- main.go
+++ main.go
@@ -5,52 +5,57 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"time"
 
 	"github.com/chromedp/chromedp"
 	"github.com/chromedp/chromedp/kb"
 )
 
-func foo(context.Context) error {
-	fmt.Println("HI")
-	return nil
+func a(msg string) chromedp.ActionFunc {
+	return func(context.Context) error {
+		fmt.Println(msg)
+		return nil
+	}
 }
 
 func main() {
-	var a chromedp.ActionFunc = foo
 	ctx, cancel := chromedp.NewContext(context.Background())
 	defer cancel()
 	// fmt.Println("Введите запрос:")
 	// scanner := bufio.NewReader(os.Stdin)
 	// query, _ := scanner.ReadString('\n')
+	var buf0 []byte
 	var buf1 []byte
 	var buf2 []byte
 	var buf3 []byte
 	var res string
 	if err := chromedp.Run(ctx,
 		chromedp.Navigate("https://www.youtube.com/"),
-		a,
+		chromedp.WaitReady("body", chromedp.ByQuery),
+		chromedp.WaitReady("input[name=search_query]", chromedp.ByQuery),
+		chromedp.Sleep(2*time.Second),
 		chromedp.Focus("input[name=search_query]"),
-		a,
+		chromedp.Sleep(2*time.Second),
+		chromedp.CaptureScreenshot(&buf0),
+		chromedp.WaitReady("body", chromedp.ByQuery),
+		chromedp.WaitReady("input[name=search_query]", chromedp.ByQuery),
 		chromedp.SendKeys("input[name=search_query]", "test", chromedp.ByQuery),
-		a,
+		chromedp.Sleep(2*time.Second),
 		chromedp.CaptureScreenshot(&buf1),
-		a,
+		chromedp.WaitReady("body", chromedp.ByQuery),
+		chromedp.WaitReady("input[name=search_query]", chromedp.ByQuery),
+		chromedp.Sleep(2*time.Second),
 		chromedp.KeyEvent(kb.Enter),
-		a,
-		chromedp.WaitVisible("yt-touch-feedback-shape", chromedp.ByQuery),
-		a,
+		chromedp.WaitReady("body", chromedp.ByQuery),
+		chromedp.Sleep(20*time.Second),
 		chromedp.CaptureScreenshot(&buf2),
-		a,
-		chromedp.WaitVisible("ytd-app", chromedp.ByQuery),
-		a,
 		chromedp.Text("#video-title", &res, chromedp.ByQuery),
-		a,
 		chromedp.CaptureScreenshot(&buf3),
-		a,
 	); err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println(res)
+	save("screen0.png", buf0)
 	save("screen1.png", buf1)
 	save("screen2.png", buf2)
 	save("screen3.png", buf3)
